1f35724c675fab8db1240d05c15e6788d9a402e0
Diego052508 <dfarmengol@uamv.edu.ni>
2026-03-22 15:27:35 -0600

    no se que estoy haciendo

---
+++ src/run/Ejercicio1.java
@@ -0,0 +1,20 @@
+package run;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un año: ");
+        int year = sc.nextInt();
+
+        if (year % 4 == 0 && year % 100 !=0)
+            System.out.println("El año " + year + " es bisiesto");
+        else
+            System.out.println("El año " + year + " no es bisiesto");
+    }
+}
+
+
